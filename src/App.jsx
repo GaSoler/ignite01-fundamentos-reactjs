fa931ce3ac3c8fcb38644f694ba787ab619e951f
Gabriel Eduardo Soler da Angela <gabriel.angela@anchieta.br>
2022-07-01 12:47:43 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import { Header } from './components/header';
 import { Post } from './Post';
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <>
       <Header />
 
-      <Post 
-        author="Gabi Gol" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus magnam optio non adipisci placeat facilis ratione officiis eum? Dolores, quidem a? Vitae dolores cumque, nisi est fugiat reiciendis placeat incidunt!" 
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Gabi Gol"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea labore minus consequatur eaque maxime commodi, tenetur maiores quo aliquam veritatis temporibus omnis, officia quia recusandae eius. Autem ipsam voluptatem cupiditate."
+          />
+          <Post 
+            author="PP"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, ea! Impedit, ullam delectus ipsa eligendi optio iusto! Laborum rerum, vel asperiores reiciendis, molestias alias a illum amet reprehenderit ducimus illo?"
+          />
+          
+        </main>
+      </div>
     </>
   )
 }
